a89e7b486759306252fb832c92e74f71e6477112
VitorMendesco <vitor.mcosta@sptech.school>
2022-09-03 11:56:15 -0300

    build: index sections finished with AOS lib animations

--- public/js/pagesJS/index.js
+++ public/js/pagesJS/index.js
@@ -17,21 +17,3 @@ function hideLogin() {
   }, 200);
 }
 // ----------------
-// display signup card and reset form
-function showSignup() {
-  document.querySelector("body").style.overflowY = "hidden"; // disable scroll
-  aside_signup.style.display = "flex";
-  form_signup.reset();
-  setTimeout(() => {
-    opacityPointer("#aside_signup", "show");
-    signup_email.focus();
-  }, 200);
-}
-// hide signup card
-function hideSignup() {
-  document.querySelector("body").style.overflowY = "auto"; // enable scroll again
-  opacityPointer("#aside_signup", "hide");
-  setTimeout(() => {
-    aside_signup.style.display = "none";
-  }, 200);
-}
