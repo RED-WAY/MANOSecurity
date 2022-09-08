be3349b04bf29bd27f3138ea19c1531ab50d183e
VitorMendesco <vitor.mcosta@sptech.school>
2022-09-08 17:40:34 -0300

    build: keyboard and button focus interaction at form buttons + img clip-path

--- public/js/global.js
+++ public/js/global.js
@@ -22,15 +22,26 @@ function hideMenu() {
 }
 
 // exit form with "escape" key
-document.addEventListener("keydown", (event) => {
+window.addEventListener("keydown", (event) => {
+  // press button in form with ENTER
+  if (
+    document.querySelector(".popup-form").display != "flex" &&
+    event.key == "Enter"
+  ) {    
+    document.querySelector(".btnForm").focus();
+    setTimeout(() => {
+      document.querySelector(".btnForm").click();
+    }, 400);
+  }
+
+  // close forms with ESC
   if (
     document.querySelector(".section-start") !== null &&
     event.key === "Escape"
   ) {
     hideLogin();
-    hideSignup();
   } else if (
-    document.querySelector("#aside_forms").style.display == "flex" &&
+    document.querySelector("#aside_forms") !== null &&
     event.key === "Escape"
   ) {
     formView(false);
