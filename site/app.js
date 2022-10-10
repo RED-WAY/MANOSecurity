e820e77fd569ee54141274866f0c173b1a451a86
VitorMendesco <vitor.mcosta@sptech.school>
2022-10-09 21:07:04 -0300

    deploy fix

--- site/app.js
+++ site/app.js
@@ -6,7 +6,7 @@ process.env.ENV = "production";
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const PORT = process.env.PORT == "development" ? 3333 : 8080;
+const PORT = process.env.ENV == "development" ? 3333 : 8080;
 
 const app = express();
 
