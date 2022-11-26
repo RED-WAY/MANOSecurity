2d635dfbd89d269a4864c7440a8a80d8085cb402
VitorMendesco <vitormendesco.js@gmail.com>
2022-11-26 14:01:01 -0300

    ignore fixed

---
+++ back/app.js
@@ -0,0 +1,44 @@
+// CHOOSE ENVIRONMENT
+// process.env.ENV = "development";
+process.env.ENV = "production";
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORT = 8082;
+
+const app = express();
+
+const indexRouter = require("./src/routes/index");
+const userRouter = require("./src/routes/users");
+const machineRouter = require("./src/routes/machine");
+const familyRouter = require("./src/routes/family");
+const accessRouter = require("./src/routes/access");
+const dashRouter = require("./src/routes/dash");
+// ADD ROUTES HERE
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/machine", machineRouter);
+app.use("/family", familyRouter);
+app.use("/access", accessRouter);
+app.use("/dash", dashRouter);
+
+// ADD ROUTES HERE TOO
+
+console.clear();
+app.listen(PORT, function () {
+  console.log(`Server running at: http://localhost:${PORT} \n
+    Application at ${process.env.ENV} process mode \n
+    \t\tIf "development" => connected to local database (MySQL - Workbench). \n
+    \t\tIf "production" => connected to remote database (SQL Server - Azure) \n
+    \t\t\tTo SWITCH ENVIRONMENT, comment or uncomment the first 2 lines at: app.js`);
+});
+
+const telegram = require("./telegram.js");
---
+++ back/src/database/config.js
@@ -0,0 +1,76 @@
+const mysql = require("mysql2");
+const sql = require("mssql");
+
+// SQL SERVER - AZURE (CLOUD)
+const sqlServerConfig = {
+  server: "svr-manosecurity.database.windows.net",
+  database: "db-azure-manosecurity",
+  user: "admin-manosecurity",
+  password: "#Gfgrupo6",
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true,
+  },
+};
+
+// MYSQL WORKBENCH (LOCAL)
+const mySqlConfig = {
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+};
+
+function executeQuery(modelQuery) {
+  if (process.env.ENV == "production") {
+    return new Promise(function (resolve, reject) {
+      sql
+        .connect(sqlServerConfig)
+        .then(function () {
+          return sql.query(modelQuery);
+        })
+        .then(function (results) {
+          console.log(results);
+          resolve(results.recordset);
+        })
+        .catch(function (error) {
+          reject(error);
+          console.log("ERROR: ", error);
+        });
+      sql.on("error", function (error) {
+        return "SQL SERVER - AZURE ERROR: ", error;
+      });
+    });
+  } else if (process.env.ENV == "development") {
+    return new Promise(function (resolve, reject) {
+      const connection = mysql.createConnection(mySqlConfig);
+      connection.connect();
+      connection.query(modelQuery, function (error, results) {
+        connection.end();
+        if (error) {
+          reject(error);
+        }
+        console.log(results);
+        resolve(results);
+      });
+      connection.on("error", function (error) {
+        return "MySQL - WORKBENCH ERROR: ", error.sqlMessage;
+      });
+    });
+  } else {
+    return new Promise(function (resolve, reject) {
+      console.log(
+        "\nENVIRONMENT (development | production) WAS NOT DEFINED AT: app.js\n"
+      );
+      reject("ENVIRONMENT NOT CONFIGURED AT: app.js");
+    });
+  }
+}
+
+module.exports = {
+  executeQuery,
+};
